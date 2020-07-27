40cc6f7cce570750af701299cfd11bca85ed067f
Arjun Aravind <arjun.aravind1998@gmail.com>
2020-07-27 13:11:06 +0530

    [Arjun] Finish style for dropdown items.

--- App.js
+++ App.js
@@ -8,17 +8,40 @@ export default function App() {
 		<View style={styles.DropdownContainer}>
 
 			<View style={styles.DropdownSearch}>
-				<TextInput style={styles.DropdownSearchText} placeholder='Search' />
-				<Icon name="search" size={25} color="#BBB" style={styles.DropdownSearchIcon} />
+				<TextInput style={styles.DropdownText} placeholder='Search' />
+				<Icon name="search" size={20} color="#BBB" style={styles.DropdownIcon} />
+			</View>
+
+			<View style={{width: '100%', marginVertical: '2%'}}></View>
+
+			<View style={styles.DropdownItemsContainer}>
+				<View style={styles.DropdownItemsFrame}>
+					<DropdownItem name='Java' title={true}/>
+					<DropdownItem name='Python' title={true}/>
+					<DropdownItem name='C++' selected={true} />	
+				</View>
 			</View>
 
 		</View>
 	)
 }
 
+export function DropdownItem(props) {
+	const iconColor = (props.selected)? 'green' : 'white'
+	const containerStyle = (props.selected)? '#F8F8F8' : 'white'
+
+	return (
+		<View style={[styles.DropdownItemContainer, {backgroundColor: containerStyle}]}>
+			<Text style={[styles.DropdownText]}>
+				{props.name}
+			</Text>
+			<Icon name="check-circle" size={20} color={iconColor} style={styles.DropdownIcon} />
+		</View>
+	)
+}
+
 const styles = StyleSheet.create({
 	DropdownContainer: {
-		backgroundColor: 'orange',
 		width: '100%',
 		paddingTop: '8%',
 		flex: 1
@@ -28,15 +51,33 @@ const styles = StyleSheet.create({
 		width: '100%',
 		flexDirection: 'row',
 		paddingVertical: '4%',
-		backgroundColor: 'white'
+		backgroundColor: 'white',
+		elevation: 4
 	},
-	DropdownSearchText: {
+	DropdownText: {
 		width: '80%',
 		marginLeft: '5%',
-		fontSize: 16,
+		fontSize: 16
+	},
+	DropdownIcon: {
+		marginLeft: '3%'
+	},
+
+	DropdownItemsContainer: {
+		width: '100%',
+		height: 100,
+		flexDirection: 'row'
+	},
+
+	DropdownItemsFrame: {
+		width: '100%',
 		backgroundColor: 'white'
 	},
-	DropdownSearchIcon: {
-		marginLeft: '3%'
+
+	DropdownItemContainer: {
+		width: '100%',
+		paddingVertical: '4%',
+		flexDirection: 'row',
+		backgroundColor: 'white'
 	}
 });
